5d0e3d4864e0ede115d8e496782cbcd1f34ed25d
Samarth-2021 <143602439+Samarth-2021@users.noreply.github.com>
2023-08-30 15:58:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+st.write("""Streamlit application
+        This Application finds the maximum number among the two numbers
+""")
+def app_logic(num_one, num_two, num_three):
+    maximum=num_one
+    if maximum < num_two:
+        maximum=num_two
+    if maximum < num_three:
+        maximum=num_three
+    
+    return maximum
 
+num_one=st.number_input("First number")
+num_two=st.number_input("Second number")
+num_three=st.number_input("Third number")
+largest=app_logic(num_one, num_two, num_three)
